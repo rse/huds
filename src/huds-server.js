7011698c7c0c2a16547594cca19f4344c5769f2a
Ralf S. Engelschall <rse@engelschall.com>
2022-04-02 10:46:27 +0200

    update year in all copyright messages

--- src/huds-server.js
+++ src/huds-server.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 /*!
 **  HUDS -- Head-Up-Display Server (HUDS)
-**  Copyright (c) 2020-2021 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**  Copyright (c) 2020-2022 Dr. Ralf S. Engelschall <rse@engelschall.com>
 **
 **  Permission is hereby granted, free of charge, to any person obtaining
 **  a copy of this software and associated documentation files (the
@@ -97,7 +97,7 @@ const HUD = {}
     if (argv.version) {
         process.stderr.write(`${my.name} ${my.version} <${my.homepage}>\n`)
         process.stderr.write(`${my.description}\n`)
-        process.stderr.write(`Copyright (c) 2020 ${my.author.name} <${my.author.url}>\n`)
+        process.stderr.write(`Copyright (c) 2020-2022 ${my.author.name} <${my.author.url}>\n`)
         process.stderr.write(`Licensed under ${my.license} <http://spdx.org/licenses/${my.license}.html>\n`)
         process.exit(0)
     }
